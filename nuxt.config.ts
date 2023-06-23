1bfe061353126bbbc1e2911a2ab09af712be6a1e
Kento Tani <k.tani0212@gmail.com>
2023-06-23 13:08:49 +0900

    headの情報を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'PATONA',
+      titleTemplate: 'PATONA',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+      htmlAttrs: {
+        lang: 'ja'
+      },
+    }
+  },
   devtools: { enabled: true }
 })
