58b277a8507f9e35c4ec871b58a20c910424ceb7
Kento Tani <k.tani0212@gmail.com>
2023-06-23 14:03:07 +0900

    add pages layouts

---
+++ components/common/app-bar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+    app
+    fixed
+    flat
+    density="compact"
+  >
+    <v-toolbar-title class="mx-0">
+      <v-row
+        dense
+        no-gutters
+        class="px-2"
+      >
+        <div class="pt-1 pl-1 text-left font-weight-bold">
+          {{ title }}
+        </div>
+      </v-row>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+const title = ref('PATONA')
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-card variant="flat">
+      <v-card-title>
+        Root Page
+      </v-card-title>
+
+      <v-card-actions>
+        <v-spacer />
+
+        <v-btn>
+          <v-icon>
+            mdi-close
+          </v-icon>Close
+        </v-btn>
+
+        <v-btn>
+          <v-icon>
+            mdi-circle
+          </v-icon>Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
